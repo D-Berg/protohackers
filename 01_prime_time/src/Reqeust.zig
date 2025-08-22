2349f1bdb93e2fcb238e3a58a0907b7fecb64eb7
D-Berg <danielfjberg@gmail.com>
2025-08-22 11:56:10 +0200

    a fresh start

--- 01_prime_time/src/Reqeust.zig
+++
@@ -1,3 +0,0 @@
-const std = @import("std");
-method: []const u8,
-number: std.json.Value,
--- tcp_server/src/main.zig
+++
@@ -1,8 +0,0 @@
-const Server = @import("root.zig").Server;
-
-pub fn main() !void {
-    const server = try Server.init("127.0.0.1", 8000);
-    defer server.deinit();
-
-    try server.listen();
-}
